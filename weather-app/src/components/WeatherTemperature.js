d09076750aaf170a971876a7cb6f30eb5898b884
nataliahn <waldenvgn@gmail.com>
2018-01-14 22:28:03 -0300

    switch weather icons

--- weather-app/src/components/WeatherTemperature.js
+++ weather-app/src/components/WeatherTemperature.js
@@ -1,5 +1,20 @@
 import React from 'react';
+import WeatherIcons from 'react-weathericons';
 
-const WeatherTemperature = () => (<div><span>12 cº</span></div>);
+const getWeatherIcon = weatherState => {
+    switch (weatherState) {
+        case "cloud":
+            return (<WeatherIcons name="cloud" size="2x"/>);
+        default:
+            return (<WeatherIcons name="wi wi-day-rain" size="2x"/>);
+    }
+};
+
+const WeatherTemperature = ({ temperature, weatherState }) => (
+    <div>
+        {getWeatherIcon(weatherState)}
+        <span>{`${temperature} Cº` } </span>
+    </div>
+);
 
 export default WeatherTemperature; 
