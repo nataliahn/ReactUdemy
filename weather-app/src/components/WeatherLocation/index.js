b785625e1cd68ecd8e97b06d8b469697779615f3
nataliahn <waldenvgn@gmail.com>
2018-01-17 00:47:10 -0300

    a dormir -_-

--- weather-app/src/components/WeatherLocation/index.js
+++ weather-app/src/components/WeatherLocation/index.js
@@ -4,9 +4,9 @@ import WeatherData from './WeatherData';
 import { SUN} from './../../constants/weathers';
 import './styles.css';
 
-const location = "London";
+const location = "Temuco, cl";
 const api_key = "4562a91e5a88f90c5c15ba8549f7530d";
-const api_weather = `http//api.openweathermap.org/data/2.5/weather?q=${location}&appid=${api_key}`;
+const api_weather = `http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${api_key}`;
 const data1 = {
     temperature : 20,
     weatherState: SUN,
@@ -23,13 +23,33 @@ class WeatherLocation extends Component {
             data: data1, 
         };
     }
+    getWeatherState = weather => {
+        return SUN;
+    }
+    getData = weather_data => {
+        const { humidity, temp } = weather_data.main;
+        const { speed } = weather_data.wind;
+        const weatherState = this.getWeatherState(this.weather);
+
+        const data = {
+            humidity,
+            temperature: temp,
+            weatherState,
+            wind: `${speed} m/s`,
+        }
+
+        return data;
+    }
 
     handleUpdateClick = () => {
-        fetch(api_weather);
-        // this.setState({
-        //     data: data2
-        // });
-        // console.log("actualizando..");
+        fetch(api_weather).then( data => {
+            console.log(data);
+            return data.json();
+        }).then(weather_data => {
+            debugger;
+            const data = this.getDat(weather_data);
+            this.setState({ data });
+        });
     }
 
     render = () => {
