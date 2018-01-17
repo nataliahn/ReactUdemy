538b335b89e92767ee1c0920ced7c24699056ac7
nataliahn <waldenvgn@gmail.com>
2018-01-17 00:12:58 -0300

    Conexión API

--- weather-app/src/components/WeatherLocation/index.js
+++ weather-app/src/components/WeatherLocation/index.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import Location from './Location';
 import WeatherData from './WeatherData';
-import { SUN } from './../../constants/weathers';
+import { SUN} from './../../constants/weathers';
 import './styles.css';
 
-const data = {
+const location = "London";
+const api_key = "4562a91e5a88f90c5c15ba8549f7530d";
+const api_weather = `http//api.openweathermap.org/data/2.5/weather?q=${location}&appid=${api_key}`;
+const data1 = {
     temperature : 20,
     weatherState: SUN,
     humidity: 10,
@@ -13,13 +16,32 @@ const data = {
 
 class WeatherLocation extends Component {
 
-    render = () => (
-    <div className='weatherLocationCont'>
-        <Location city = {'Wallmapu'} />
-        <WeatherData data={data}/>
-    </div>
-    );
-    
+    constructor() {
+        super();
+        this.state = {
+            city: 'WallMapu!',
+            data: data1, 
+        };
+    }
+
+    handleUpdateClick = () => {
+        fetch(api_weather);
+        // this.setState({
+        //     data: data2
+        // });
+        // console.log("actualizando..");
+    }
+
+    render = () => {
+        const { city, data } = this.state;
+        return (
+        <div className='weatherLocationCont'>
+            <Location city = {city} />
+            <WeatherData data={data}/>
+            <button onClick={this.handleUpdateClick} >Actualizar</button>    
+        </div>);
+    };
+
 };
 
 export default WeatherLocation;
