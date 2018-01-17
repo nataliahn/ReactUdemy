74222c76366c6492b0dd7bc0df2aa8f9e186ec68
nataliahn <waldenvgn@gmail.com>
2018-01-16 21:28:48 -0300

    styling

--- weather-app/src/components/WeatherLocation/WeatherData/WeatherTemperature.js
+++ weather-app/src/components/WeatherLocation/WeatherData/WeatherTemperature.js
@@ -8,6 +8,7 @@ import { CLOUD,
     SNOW, 
     WINDY
 } from './../../../constants/weathers';
+import './styles.css';
 
 const stateToIconName = weatherState => {
     switch (weatherState) {
@@ -29,13 +30,14 @@ const stateToIconName = weatherState => {
 };
 
 const getWeatherIcon = weatherState => {
-    return (<WeatherIcons name={stateToIconName(weatherState)} size="2x" />);
+    return (<WeatherIcons className='wicon' name={stateToIconName(weatherState)} size="4x" />);
 };
 
 const WeatherTemperature = ({ temperature, weatherState }) => (
-    <div>
+    <div className='weatherTemperatureCont'>
         {getWeatherIcon(weatherState)}
-        <span>{`${temperature} Cº` } </span>
+        <span className='temperature'>{`${temperature}` } </span>
+        <span className='temperatureType'>Cº</span>
     </div>
 );
 
